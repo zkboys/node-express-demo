4416b78befd8f17582efedab981fb53cc9e03445
wangshubin <876213774@qq.com>
2016-09-04 23:38:28 +0800

    模拟数据

--- routes.js
+++ routes.js
@@ -22,139 +22,154 @@ router.get('/data.json', function (req, res, next) {
     var endDate = req.query.endDate;
     var nextCursor = req.query.nextCursor;
     // TODO 根据条件查询数据
+
+    function rn(Min, Max) {
+        var Range = Max - Min;
+        var Rand = Math.random();
+        return (Min + Math.round(Rand * Range));
+    }
+
+    function getRNData() {
+        var arr = [0, rn(0, 5), 0, rn(0, 5), rn(0, 5), 0, 0];
+        arr.sort(function () {
+            return 0.5 - Math.random()
+        })
+        return arr;
+    }
+
     var data = {
         nextCursor: 1, // 滚动加载，是否有下一页的标记以及后端查询下一页的开始条件, 0 表示没有下一页了。
         result: [
             {
                 name: startDate,
-                data: [0, 1, 2, 3, 4, 5, 1]
+                data: getRNData()
             },
             {
                 name: endDate,
-                data: [0, 0, 1, 0, 2, 0, 3]
+                data: getRNData()
             },
             {
                 name: company,
-                data: [0, 3, 1, 0, 0, 1, 0]
+                data: getRNData()
             },
             {
                 name: '张三',
-                data: [0, 1, 2, 3, 0, 0, 1]
+                data: getRNData()
             },
             {
                 name: '李四',
-                data: [0, 0, 1, 0, 2, 0, 3]
+                data: getRNData()
             },
             {
                 name: '王五',
-                data: [0, 3, 1, 0, 0, 1, 0]
+                data: getRNData()
             },
             {
                 name: '张三',
-                data: [0, 1, 2, 3, 0, 0, 1]
+                data: getRNData()
             },
             {
                 name: '李四',
-                data: [0, 0, 1, 0, 2, 0, 3]
+                data: getRNData()
             },
             {
                 name: '王五',
-                data: [0, 3, 1, 0, 0, 1, 0]
+                data: getRNData()
             },
             {
                 name: '张三',
-                data: [0, 1, 2, 3, 0, 0, 1]
+                data: getRNData()
             },
             {
                 name: '李四',
-                data: [0, 0, 1, 0, 2, 0, 3]
+                data: getRNData()
             },
             {
                 name: '王五',
-                data: [0, 3, 1, 0, 0, 1, 0]
+                data: getRNData()
             },
             {
                 name: '张三',
-                data: [0, 1, 2, 3, 0, 0, 1]
+                data: getRNData()
             },
             {
                 name: '李四',
-                data: [0, 0, 1, 0, 2, 0, 3]
+                data: getRNData()
             },
             {
                 name: '王五',
-                data: [0, 3, 1, 0, 0, 1, 0]
+                data: getRNData()
             },
             {
                 name: '张三',
-                data: [0, 1, 2, 3, 0, 0, 1]
+                data: getRNData()
             },
             {
                 name: '李四',
-                data: [0, 0, 1, 0, 2, 0, 3]
+                data: getRNData()
             },
             {
                 name: '王五',
-                data: [0, 3, 1, 0, 0, 1, 0]
+                data: getRNData()
             },
             {
                 name: '张三',
-                data: [0, 1, 2, 3, 0, 0, 1]
+                data: getRNData()
             },
             {
                 name: '李四',
-                data: [0, 0, 1, 0, 2, 0, 3]
+                data: getRNData()
             },
             {
                 name: '王五',
-                data: [0, 3, 1, 0, 0, 1, 0]
+                data: getRNData()
             },
             {
                 name: '张三',
-                data: [0, 1, 2, 3, 0, 0, 1]
+                data: getRNData()
             },
             {
                 name: '李四',
-                data: [0, 0, 1, 0, 2, 0, 3]
+                data: getRNData()
             },
             {
                 name: '王五',
-                data: [0, 3, 1, 0, 0, 1, 0]
+                data: getRNData()
             },
             {
                 name: '张三',
-                data: [0, 1, 2, 3, 0, 0, 1]
+                data: getRNData()
             },
             {
                 name: '李四',
-                data: [0, 0, 1, 0, 2, 0, 3]
+                data: getRNData()
             },
             {
                 name: '王五',
-                data: [0, 3, 1, 0, 0, 1, 0]
+                data: getRNData()
             }, {
                 name: '张三',
-                data: [0, 1, 2, 3, 0, 0, 1]
+                data: getRNData()
             },
             {
                 name: '李四',
-                data: [0, 0, 1, 0, 2, 0, 3]
+                data: getRNData()
             },
             {
                 name: '王五',
-                data: [0, 3, 1, 0, 0, 1, 0]
+                data: getRNData()
             },
             {
                 name: '张三',
-                data: [0, 1, 2, 3, 0, 0, 1]
+                data: getRNData()
             },
             {
                 name: '李四',
-                data: [0, 0, 1, 0, 2, 0, 3]
+                data: getRNData()
             },
             {
                 name: '王五',
-                data: [0, 3, 1, 0, 0, 1, 0]
+                data: getRNData()
             },
         ]
     };

3f85b2474f1415ca266bb6908e227b02d7bdf7b5
wangshubin <876213774@qq.com>
2016-09-04 23:16:43 +0800

    年月可选

--- routes.js
+++ routes.js
@@ -26,15 +26,15 @@ router.get('/data.json', function (req, res, next) {
         nextCursor: 1, // 滚动加载，是否有下一页的标记以及后端查询下一页的开始条件, 0 表示没有下一页了。
         result: [
             {
-                name: '张三',
+                name: startDate,
                 data: [0, 1, 2, 3, 4, 5, 1]
             },
             {
-                name: '李四',
+                name: endDate,
                 data: [0, 0, 1, 0, 2, 0, 3]
             },
             {
-                name: '王五',
+                name: company,
                 data: [0, 3, 1, 0, 0, 1, 0]
             },
             {
